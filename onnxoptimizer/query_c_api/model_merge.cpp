7a95c2f7174b71db1ea87dceaeef203c71a66303
yangxinyu7427 <742707786@qq.com>
2023-11-15 19:09:17 +0800

    add add_model_prefix func

--- onnxoptimizer/query_c_api/model_merge.cpp
+++ onnxoptimizer/query_c_api/model_merge.cpp
@@ -8,17 +8,26 @@
 
 namespace ONNX_NAMESPACE {
 namespace optimization {
+
+void add_model_prefix(
+    ModelProto* model,
+    std::string& prefix,
+    ModelProto* model_prefixed){
+
+}
+
+
 void model_merge(
-    const ModelProto& m1,
-    const ModelProto& m2,
-    const std::string& mp_name1,
-    const std::string& mp_name2,
-    const ModelProto& mp_merged){
-  if (m1.ir_version()!=m2.ir_version()){
+    ModelProto* m1,
+    ModelProto* m2,
+    std::string& mp_name1,
+    std::string& mp_name2,
+    ModelProto* mp_merged){
+  if (m1->ir_version()!=m2->ir_version()){
     std::cerr << "Warning: onnx ir versions are different! " << std::endl;
   }
   std::map<std::string,::onnx::OperatorSetIdProto> map={};
-  for(const auto & it : m1.opset_import()){
+  for(const auto & it : m1->opset_import()){
     if(map.find(it.domain()) == map.end()){
       map[it.domain()]=it;
       if("ai.onnx.ml"!=it.domain()){
@@ -35,7 +44,7 @@ void model_merge(
     }
   }
 
-  for(const auto & it : m2.opset_import()){
+  for(const auto & it : m2->opset_import()){
     if(map.find(it.domain()) == map.end()){
       map[it.domain()]=it;
       if("ai.onnx.ml"!=it.domain()){
@@ -51,6 +60,14 @@ void model_merge(
       }
     }
   }
+  std::vector<OperatorSetIdProto> opset_import_list;
+  for(const auto & it:map){
+    opset_import_list.push_back(it.second);
+  }
+
+  ModelProto m1_prefix;
+  add_model_prefix(m1,mp_name1,&m1_prefix);
+
 }
 }
 }
