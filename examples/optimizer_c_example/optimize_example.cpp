9fedf3c6a6d0f13d8c56bc869bf83fbfe6f6a8bf
yangxinyu7427 <742707786@qq.com>
2024-04-21 19:47:44 +0800

    change model

--- examples/optimizer_c_example/optimize_example.cpp
+++ examples/optimizer_c_example/optimize_example.cpp
@@ -17,13 +17,21 @@ int main(int argc, char* argv[]) {
 //  std::string prefix2="model_linear";
 //  auto model_opted=onnx::optimization::OptimizeWithModels(model1,model2, prefix1,prefix2);
 //  onnx::optimization::saveModel(&model_opted,"../examples/onnx_output_model/model_opted.onnx");
-  std::string path1="../examples/onnx_input_model/model_lr.onnx";
-  std::string path2="../examples/onnx_input_model/model_linear.onnx";
-  std::string pre1="model_lr_1_";
-  std::string pre2="model_linear_1_";
-  std::string pre3="model_lr_2_";
+//  std::string path1="../examples/onnx_input_model/model_lr.onnx";
+//  std::string path2="../examples/onnx_input_model/model_linear.onnx";
+
+  std::string path1="../examples/onnx_input_model/news_nb.onnx";
+  std::string path2="../examples/onnx_input_model/news_lr.onnx";
+  std::string path3="../examples/onnx_input_model/news_sgd.onnx";
+//  std::string pre1="model_lr_1_";
+//  std::string pre2="model_linear_1_";
+  std::string pre1="nb_1_";
+  std::string pre2="lr_1_";
+  std::string pre3="sgd_1_";
   std::string pre4="";
+  //std::string out_path="../examples/onnx_output_model/model_opted.onnx";
   std::string out_path="../examples/onnx_output_model/model_opted.onnx";
   optimize_with_model_path(path1,path2,pre1,pre2,out_path);
-  optimize_with_model_path(out_path,path1,pre4,pre3,out_path);
+  optimize_with_model_path(path3,out_path,pre3,pre4,out_path);
+  //optimize_with_model_path(out_path,path1,pre4,pre3,out_path);
 }
--- examples/optimizer_c_example/predicate_example.cpp
+++ examples/optimizer_c_example/predicate_example.cpp
@@ -6,14 +6,15 @@
 
 int main(int argc, char* argv[]) {
 
+  //std::string path="../examples/onnx_output_model/model_opted.onnx";
   std::string path="../examples/onnx_output_model/model_opted.onnx";
   std::string predicate1="Greater";
   std::string predicate2="Or";
   std::string predicate3="And";
   std::string type="int";
-  std::string type2="bool";
-  std::string name1="model_lr_1";
-  std::string name2="model_linear_1";
+  std::string type2="string";
+  std::string name1="sgd_1";
+  std::string name2="lr_1";
   std::string name3="model_lr_1_model_linear_1";
   std::string name4="model_lr_2";
   std::string name5="model_lr_1_model_linear_1_model_lr_2";
@@ -25,7 +26,7 @@ int main(int argc, char* argv[]) {
 //  merge_single_model_with_predicate(path,predicate4,type,name3);
 
   merge_single_model_with_predicate(path,predicate1,type,name1,1);
-  merge_single_model_with_predicate(path,predicate1,type,name1,2);
+  //merge_single_model_with_predicate(path,predicate1,type,name1,2);
   merge_single_model_with_predicate(path,predicate1,type,name2,1);
   merge_double_models_with_predicate(path,predicate3,name1,name2);
 //  merge_single_model_with_predicate(path,predicate1,type,name4,1);
