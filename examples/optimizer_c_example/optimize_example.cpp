395346f5c2d115c2056b0843c80e7f9c39c522f0
yangxinyu7427 <742707786@qq.com>
2024-04-28 19:12:54 +0800

    save model output_label

--- examples/optimizer_c_example/optimize_example.cpp
+++ examples/optimizer_c_example/optimize_example.cpp
@@ -8,23 +8,23 @@
 #include "onnxoptimizer/optimize_c_api/optimize_c_api.h"
 
 int main(int argc, char* argv[]) {
-//  ONNX_NAMESPACE::ModelProto model1,model2;
-//  onnx::optimization::loadModel(&model1, "../examples/onnx_input_model/model_lr.onnx", true);
-//  onnx::checker::check_model(model1);
-//  onnx::optimization::loadModel(&model2, "../examples/onnx_input_model/model_linear.onnx", true);
-//  onnx::checker::check_model(model2);
-//  std::string prefix1="model_lr";
-//  std::string prefix2="model_linear";
-//  auto model_opted=onnx::optimization::OptimizeWithModels(model1,model2, prefix1,prefix2);
-//  onnx::optimization::saveModel(&model_opted,"../examples/onnx_output_model/model_opted.onnx");
-//  std::string path1="../examples/onnx_input_model/model_lr.onnx";
-//  std::string path2="../examples/onnx_input_model/model_linear.onnx";
+  //  ONNX_NAMESPACE::ModelProto model1,model2;
+  //  onnx::optimization::loadModel(&model1, "../examples/onnx_input_model/model_lr.onnx", true);
+  //  onnx::checker::check_model(model1);
+  //  onnx::optimization::loadModel(&model2, "../examples/onnx_input_model/model_linear.onnx", true);
+  //  onnx::checker::check_model(model2);
+  //  std::string prefix1="model_lr";
+  //  std::string prefix2="model_linear";
+  //  auto model_opted=onnx::optimization::OptimizeWithModels(model1,model2, prefix1,prefix2);
+  //  onnx::optimization::saveModel(&model_opted,"../examples/onnx_output_model/model_opted.onnx");
+  //  std::string path1="../examples/onnx_input_model/model_lr.onnx";
+  //  std::string path2="../examples/onnx_input_model/model_linear.onnx";
 
   std::string path1="../examples/onnx_input_model/news_nb.onnx";
   std::string path2="../examples/onnx_input_model/news_lr.onnx";
   std::string path3="../examples/onnx_input_model/news_sgd.onnx";
-//  std::string pre1="model_lr_1_";
-//  std::string pre2="model_linear_1_";
+  //  std::string pre1="model_lr_1_";
+  //  std::string pre2="model_linear_1_";
   std::string pre1="nb_1_";
   std::string pre2="lr_1_";
   std::string pre3="sgd_1_";
