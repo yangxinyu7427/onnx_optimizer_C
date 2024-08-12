603fd7db0a53e771397a51630d1f32c6c77b44f1
yxy <742707786@qq.com>
2024-08-12 15:55:23 +0800

    fix: fix head file compile bug

--- onnxoptimizer/optimize_c_api/optimize_c_api.h
+++ onnxoptimizer/optimize_c_api/optimize_c_api.h
@@ -4,7 +4,7 @@
 
 #ifndef ONNX_OPTIMIZER_OPTIMIZE_C_API_H
 #define ONNX_OPTIMIZER_OPTIMIZE_C_API_H
-
+#include <vector>
 void optimize_with_model_path(std::string& mp_in_path1,
                               std::string& mp_in_path2, std::string& mp_name1,
                               std::string& mp_name2, std::string& mp_out_path);
