d663544dae7d80fbc77d36768c37fc53f1bd2688
ddding-z <51275903023@stu.ecnu.edu.com>
2024-12-23 00:47:24 +0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 删除旧的 build 目录
+rm -rf build
+
+# 创建新的 build 目录并配置 Debug 构建
+cmake -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+# 构建项目，Debug 模式，先清理再并行编译
+cmake --build build --config Debug --clean-first --parallel 10
